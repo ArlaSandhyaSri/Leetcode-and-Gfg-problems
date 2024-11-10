623d02cf3e1171f0169379156709ef96e5c6cbe2
ARLA.SANDHYA SRI <140948560+ArlaSandhyaSri@users.noreply.github.com>
2024-11-10 18:46:56 +0530

    Time: 2 ms (99.84%) | Memory: 43.2 MB (40.45%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.java
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -12,7 +12,7 @@ class Solution {
         }
         for(int i=index;i<candidates.length;i++){
             if(i>index && candidates[i]==candidates[i-1]) continue;
-            if(candidates[i]>target) break;
+            if(candidates[i]>target) return;
             ar.add(candidates[i]);
             combination(i+1,candidates,target-candidates[i],ar,finalans);
             ar.remove(ar.size()-1);

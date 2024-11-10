77502e9b0e6aa24a58ef8e3718789ab44f7ec6c9
ARLA.SANDHYA SRI <140948560+ArlaSandhyaSri@users.noreply.github.com>
2024-11-10 19:59:21 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (81.23%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -30,7 +30,8 @@ class Solution {
         //Arrays.sort(nums);
 
       // Better --Counting
-      int z=0;
+
+      /*int z=0;
       int o=0;
       int t=0;
       int l=0;
@@ -56,7 +57,39 @@ class Solution {
       }
       for(int i=z+o;i<o+z+t;i++){
         nums[i]=2;
+      }*/
+      
+      //Using Dutch National Flag Algo - optimal
+
+     /* In this 0---low-1 -----> 0
+      low----mid-1 ------->1
+      mid--high ---- > random values //now sorting this part
+      high+1-----n-1 ----->2*/
+
+      int low=0;
+      int mid=0;
+      int high=nums.length-1;
+
+      while(mid<=high){
+        if(nums[mid]==0){
+            swap(nums,low,mid);
+            low++;
+            mid++;
+        }
+        else if(nums[mid]==1){
+            mid++;
+        }
+        else{
+            swap(nums,mid,high);
+            high--; //here in above low 0 case we know that till low it is solved and till mid also solved so we can the that swapped one will be 1, but here we dont know that where number in high is 1 or not so should not increment mid;
+        }
       }
+
+    }
+    static void swap(int nums[],int l,int r){
+        int temp=nums[l];
+        nums[l]=nums[r];
+        nums[r]=temp;
 
     }
 }

fe4b7c53bed937393693f19c0cba062cee67d784
ARLA.SANDHYA SRI <140948560+ArlaSandhyaSri@users.noreply.github.com>
2024-11-11 16:24:11 +0530

    Time: 2 ms (78.86%) | Memory: 53.1 MB (30.99%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -41,7 +41,7 @@ class Solution {
 
         //Better
 
-        HashMap<Integer,Integer> mp = new HashMap<>();
+        /*HashMap<Integer,Integer> mp = new HashMap<>();
         for(int i=0;i<nums.length;i++){
             mp.put(nums[i],mp.getOrDefault(nums[i],0)+1);
         }
@@ -49,6 +49,32 @@ class Solution {
             if(entry.getValue()>nums.length/2){
                 return entry.getKey();
             }
+        }
+        return -1;*/
+
+        //Optimal --> using moore voting algo
+        int element=-1;
+        int count=0;
+        for(int i=0;i<nums.length;i++){
+            if(count==0){
+                element=nums[i];
+                count++;
+            }
+            else if(element==nums[i]){
+                count++;
+            }
+            else{
+                count--;
+            }
+        }
+        int c=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==element){
+                c++;
+            }
+        }
+        if(c>nums.length/2){
+            return element;
         }
         return -1;
     }

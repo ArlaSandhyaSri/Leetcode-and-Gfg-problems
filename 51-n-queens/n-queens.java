9119cf14e84821333b114a4bfd98dc188092bc9e
ARLA.SANDHYA SRI <140948560+ArlaSandhyaSri@users.noreply.github.com>
2024-11-17 17:09:58 +0530

    Time: 6 ms (21.21%) | Memory: 44.5 MB (95.02%) - LeetSync

--- 51-n-queens/n-queens.java
+++ 51-n-queens/n-queens.java
@@ -1,5 +1,5 @@
 class Solution {
-    public List<List<String>> solveNQueens(int n) {
+    /*public List<List<String>> solveNQueens(int n) {
 
         //Approach-1 time-->O(N!*N)  space-->O(N2)
         String board[][]=new String[n][n];
@@ -31,7 +31,7 @@ class Solution {
             return;
         }
         for(int row=0;row<n;row++){
-            if(NoHarm(row,col,n,board)){
+           if(NoHarm(row,col,n,board)){
                 board[row][col]="Q";
                 SolveNQueens(col+1,ans,temp,n,board);
                 board[row][col]=".";
@@ -39,6 +39,7 @@ class Solution {
         }
     }
     static boolean NoHarm(int row,int col,int n,String board[][]){
+        //Upper diagonal
         int dirow=row;
         int dicol=col;
         while(row>=0 && col>=0){
@@ -48,6 +49,7 @@ class Solution {
             row--;
             col--;
         }
+        //left
         row=dirow;
         col=dicol;
         while(col>=0){
@@ -56,6 +58,7 @@ class Solution {
             }
             col--;
         }
+        //lower diagonal
         row=dirow;
         col=dicol;
         while(col>=0 && row<n){
@@ -66,5 +69,55 @@ class Solution {
             row++;
         }
         return true;
+    }*/
+
+    //Approach-2 (Hasing method)
+    public List<List<String>> solveNQueens(int n) {
+
+        //Approach-1 time-->O(N!*N)  space-->O(N)
+        String board[][]=new String[n][n];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                board[i][j]=".";
+            }
+        }
+        List<List<String>> ans=new ArrayList<>();
+        boolean rowcheck[]=new boolean[n];
+        boolean upperdiag[]=new boolean[2*n-1];
+        boolean lowerdiag[]=new boolean[2*n-1];
+        SolveNQueens(0,ans,new ArrayList<>(),n,board,rowcheck,upperdiag,lowerdiag);
+        return ans;
+        
+    }
+    static void SolveNQueens(int col,List<List<String>> ans,List<String>temp,int n,String board[][],boolean rowcheck[],boolean upperdiag[],boolean lowerdiag[]){
+        if(col==n){
+           List<String>t = new ArrayList<>();
+            for(int i=0;i<n;i++){
+                String str="";
+                for(int j=0;j<n;j++){
+                    str+=board[i][j];
+
+                }
+                t.add(str);
+            }
+            if(!ans.contains(t)){
+                ans.add(new ArrayList<>(t));
+
+            }
+            return;
+        }
+        for(int row=0;row<n;row++){
+            if(rowcheck[row]==false && lowerdiag[row+col]==false && upperdiag[(n-1)+col-row]==false){
+                board[row][col]="Q";
+                rowcheck[row]=true;
+                lowerdiag[row+col]=true;
+                upperdiag[(n-1)+col-row]=true;
+                SolveNQueens(col+1,ans,temp,n,board,rowcheck,upperdiag,lowerdiag);
+                board[row][col]=".";
+                rowcheck[row]=false;
+                lowerdiag[row+col]=false;
+                upperdiag[(n-1)+col-row]=false;
+            }
+        }
     }
 }

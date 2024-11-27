04c186e9c12f93dff0c2ab77bcbfbae55609d426
ARLA.SANDHYA SRI <140948560+ArlaSandhyaSri@users.noreply.github.com>
2024-11-27 11:19:29 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (28.90%) - LeetSync

--- 812-rotate-string/rotate-string.java
+++ 812-rotate-string/rotate-string.java
@@ -3,7 +3,7 @@ class Solution {
         if(s.length()!=goal.length()){
             return false;
         }
-        return (s+s).contains(goal);
+        return (s+s).contains(goal);  //We can check by merging string it will form abcdeabcde if goal is visible in s+s we can can goal will obtain after some rotations.
         
     }
 }

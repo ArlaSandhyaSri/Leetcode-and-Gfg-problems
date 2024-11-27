f5a2024354ba006e9d1f542c6fbeb2b84b7a1593
ARLA.SANDHYA SRI <140948560+ArlaSandhyaSri@users.noreply.github.com>
2024-11-27 10:08:34 +0530

    Time: 20 ms (5.34%) | Memory: 45.5 MB (5.66%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -1,32 +1,5 @@
 class Solution {
     public String reverseWords(String s) {
-        /*String ans="";
-        String temp="";
-        char ch;
-        for(int i=0;i<s.length();i++){
-            ch=s.charAt(i);
-            if(ch!=' '){
-                temp+=ch;
-            }else if(ch==' '){
-                if(temp!=""){ //If duplicate spaces encountered we are ignoring that through this step
-                    if(ans==""){
-                        ans=temp;
-                    }else{
-                        ans=temp+' '+ans;
-                    }
-                    temp="";
-                }
-            }
-        }
-
-        if(!temp.equals("")){
-            if(!ans.equals("")){
-                ans=temp+' '+ans;
-            }else{
-                ans=temp;
-            }
-        }
-        return ans;*/
         String temp="";
         String ans="";
         for(int i=0;i<s.length();i++){

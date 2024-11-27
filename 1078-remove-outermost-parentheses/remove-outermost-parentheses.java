06a00bf378ab1c52ce28dc3e257fd1a9c93cb00d
ARLA.SANDHYA SRI <140948560+ArlaSandhyaSri@users.noreply.github.com>
2024-11-27 09:51:20 +0530

    Time: 2 ms (98.49%) | Memory: 42.1 MB (69.74%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.java
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.java
@@ -1,33 +1,17 @@
 class Solution {
     public String removeOuterParentheses(String s) {
-        /*StringBuilder sb = new StringBuilder();
-        int count=0;
-        for(char ch:s.toCharArray()){
-            if(ch=='('){
-                if(count>0){ //if count>0 then it is not outermost one.
-                    sb.append(ch);
-                }
-                count++;
-            }else{
-                count--;
-                if(count>0){
-                    sb.append(ch);
-                }
-            }
-        }
-        return sb.toString();*/
         StringBuilder sb = new StringBuilder();
         int countofParanthesis=0;
         for(char ch:s.toCharArray()){
             if(ch=='('){
-                if(countofParanthesis>0){
+                if(countofParanthesis>0){ //if count>0 then it is not an outer paranthesis
                     sb.append(ch);
                 }
                 countofParanthesis++;
             }
             else{
                 countofParanthesis--;
-                if(countofParanthesis>0){
+                if(countofParanthesis>0){  //Even after decrementing count>0 then it is not closing of outer paranthesis.
                     sb.append(ch);
                 }
             }

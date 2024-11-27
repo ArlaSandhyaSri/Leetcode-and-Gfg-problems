84c12b490fe3b7fdc8c39eb6b3e342cda9d34000
ARLA.SANDHYA SRI <140948560+ArlaSandhyaSri@users.noreply.github.com>
2024-11-27 10:19:50 +0530

    Time: 19 ms (6.46%) | Memory: 45.8 MB (5.66%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -2,7 +2,7 @@ class Solution {
     public String reverseWords(String s) {
         String temp="";
         String ans="";
-        for(int i=0;i<s.length();i++){
+        /*for(int i=0;i<s.length();i++){
             char ch=s.charAt(i);
             if(ch!=' '){
                 temp+=ch;
@@ -24,6 +24,36 @@ class Solution {
             }
             else{
                 ans=temp;
+            }
+        }
+        return ans;*/
+        int left=0;
+        int right=s.length()-1;
+        while(left<=right){
+            char ch=s.charAt(left);
+            if(ch!=' '){
+                temp+=ch;
+            }
+            else if(ch==' '){
+                if(!temp.equals("")){
+                    if(!ans.equals("")){
+                        ans=temp+' '+ans;
+                    }
+                
+                    else{
+                        ans=temp;
+                    }
+                }
+                temp="";
+            }
+            left++;
+        }
+
+        if(!temp.equals("")){
+            if(ans.equals("")){
+                ans=temp;
+            }else{
+                ans=temp+' '+ans;
             }
         }
         return ans;

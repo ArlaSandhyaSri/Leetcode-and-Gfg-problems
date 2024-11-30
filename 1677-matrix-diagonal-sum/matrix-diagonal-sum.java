10d594b16ed2db011eab01fbecb0e40632e598d2
ARLA.SANDHYA SRI <140948560+ArlaSandhyaSri@users.noreply.github.com>
2024-11-30 12:30:46 +0530

    Time: 1 ms (29.73%) | Memory: 45 MB (5.46%) - LeetSync

---
+++ 1677-matrix-diagonal-sum/matrix-diagonal-sum.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int diagonalSum(int[][] mat) {
+        int rows=mat.length;
+        int columns = mat[0].length;
+        int prim_sum=0;
+
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<columns;j++){
+                if(i==j){
+                    prim_sum+=mat[i][j];
+                }
+            }
+        }
+        int sec_sum=0;
+        int r=0;
+        int c=columns-1;
+        while(r<rows && c>=0){
+            if(r!=c){
+                sec_sum+=mat[r][c];
+            }
+            r++;
+            c--;
+        }
+        return prim_sum+sec_sum;
+        
+    }
+}

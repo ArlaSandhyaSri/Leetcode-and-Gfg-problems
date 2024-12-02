45bf75e0f82aa6adbe46797e784346355642d279
ARLA.SANDHYA SRI <140948560+ArlaSandhyaSri@users.noreply.github.com>
2024-12-02 14:51:27 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (25.92%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,6 +1,6 @@
 class Solution {
     public int strStr(String haystack, String needle) {
-        if(needle.length()==0){
+        /*if(needle.length()==0){
             return -1;
         }if(needle.length()>haystack.length()){
             return -1;
@@ -21,7 +21,54 @@ class Solution {
                 return i;
             }
         }
+        return -1;*/
+
+        //Using KMP
+        int n=haystack.length();
+
+        int m=needle.length();
+        int lps[]=new int[m];
+        int len=0;
+        lps[0]=0;
+        int i=1;
+        while(i<m){
+            if(needle.charAt(i)==needle.charAt(len)){
+                len++;
+                lps[i]=len;
+                i++;
+            }
+            else{
+                if(len!=0){
+                    len=lps[len-1];
+                }
+                else{
+                    lps[i]=0;
+                    i++;
+                }
+            }
+        }
+
+        int j=0;
+        int k=0;
+        while(j<haystack.length()){
+            if(haystack.charAt(j)==needle.charAt(k)){
+                j++;
+                k++;
+                if(k==needle.length()){
+                    return j-k;
+                }
+            }else{
+                if(k!=0){
+                    k=lps[k-1];
+                }
+                else{
+                    j++;
+                }
+            }
+
+        }
         return -1;
+
         
     }
 }

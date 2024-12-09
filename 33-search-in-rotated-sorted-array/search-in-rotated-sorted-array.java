0b4150dee6349b3d31082b77103d866da9724687
ARLA.SANDHYA SRI <140948560+ArlaSandhyaSri@users.noreply.github.com>
2024-12-09 12:26:05 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (10.17%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -8,31 +8,7 @@ class Solution {
             }else{
                 i++;
             }
-        }
-        return -1;*/
-       /* int start=0;
-        int end=nums.length-1;
-        while(start<=end){
-            int mid=(start+end)/2;
-            if(nums[mid]==target){
-                return mid;
-            }
-            if(nums[start]<=nums[mid]){
-                if(nums[start]<=target && nums[mid]>target){
-                    end=mid-1;
-                }else{
-                    start=mid+1;
-                }
-            }else{
-                if(nums[mid]<target && target<=nums[end]){
-                    start=mid+1;
-                }else{
-                    end=mid-1;
-                }
-            }
-        }
-        return -1;*/
-
+        }*/
         int start=0;
         int end=nums.length-1;
         while(start<=end){

b8de65d0b5dbd67c8067615405d19544c9571084
ARLA.SANDHYA SRI <140948560+ArlaSandhyaSri@users.noreply.github.com>
2024-12-23 17:28:36 +0530

    Time: 4 ms (76.69%) | Memory: 43.3 MB (68.00%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -39,24 +39,7 @@ class Solution {
             }
         }
         return true;*/
-       /* if(s.length()!=t.length()){
-            return false;
-        }
-
-        int freq[]=new int[26];
-        for(int i=0;i<s.length();i++){
-            freq[s.charAt(i) - 'a']++;
-        }
-        for(int i=0;i<t.length();i++){
-            freq[t.charAt(i) - 'a']--;
-        }
-        for(int i=0;i<26;i++){
-            if(freq[i]!=0){
-                return false;
-            }
-        }
-        return true;*/
-
+       
         
     }
 }

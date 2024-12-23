f71437acffa6be77cc81e5c6151e51cae30bcb0d
ARLA.SANDHYA SRI <140948560+ArlaSandhyaSri@users.noreply.github.com>
2024-12-23 16:53:34 +0530

    Time: 2 ms (98.62%) | Memory: 42.6 MB (32.29%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.java
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.java
@@ -21,14 +21,14 @@ class Solution {
         int count=0;
         for(char ch:s.toCharArray()){
             if(ch=='('){
-                if(count!=0){
+                if(count>0){
                     sb.append(ch);
                 }
                 count++;
             }
             else{
                 count--;
-                if(count!=0){
+                if(count>0){
                     sb.append(ch);
                 }
 

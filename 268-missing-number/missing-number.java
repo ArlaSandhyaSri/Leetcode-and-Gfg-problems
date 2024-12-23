ed33c9803eda69273c299f199aeaf30d76d745d6
ARLA.SANDHYA SRI <140948560+ArlaSandhyaSri@users.noreply.github.com>
2024-12-23 16:29:08 +0530

    Time: 7 ms (6.26%) | Memory: 45.3 MB (56.71%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -1,5 +1,16 @@
 class Solution {
     public int missingNumber(int[] nums) {
+        int n=nums.length;
+        HashSet<Integer> hs = new HashSet<>();
+        for(int i=0;i<nums.length;i++){
+            hs.add(nums[i]);
+        }
+        for(int i=1;i<=n;i++){
+            if(!(hs.contains(i))){
+                return i;
+            }
+        }
+        return 0;
         //Optimal1
         /*int n = nums.length;
         int sum=(n*(n+1))/2;
@@ -8,8 +19,9 @@ class Solution {
             sum_a=sum_a+nums[i];
         }
         return sum-sum_a;*/
-
-        int XOR_M=0;
+        
+         //Optimal-2
+        /*int XOR_M=0;
         int XOR_S=0;
         for(int i=0;i<nums.length;i++){
             XOR_S^=nums[i];
@@ -17,6 +29,6 @@ class Solution {
         for(int i=0;i<=nums.length;i++){
             XOR_M^=i;
         }
-        return XOR_S^XOR_M;
+        return XOR_S^XOR_M;*/
     }
 }

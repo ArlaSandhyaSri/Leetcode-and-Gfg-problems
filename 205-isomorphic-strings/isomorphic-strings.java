9c2208c4cf6012c9013ae11a70ed2267c7f8b5b3
ARLA.SANDHYA SRI <140948560+ArlaSandhyaSri@users.noreply.github.com>
2024-12-23 17:22:27 +0530

    Time: 11 ms (69.73%) | Memory: 42.9 MB (13.04%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -5,6 +5,27 @@ class Solution {
         }
         HashMap<Character,Character> mp = new HashMap<>();
         for(int i=0;i<s.length();i++){
+            char ch1=s.charAt(i);
+            char ch2=t.charAt(i);
+            if(mp.containsKey(ch1)){
+                if(mp.get(ch1)!=ch2){
+                    return false;
+                }
+            }
+            else if(mp.containsValue(ch2)){
+                return false;
+            }
+            else{
+                mp.put(ch1,ch2);
+            }
+        }
+        return true;
+        /*
+        if(s.length()!=t.length()){
+            return false;
+        }
+        HashMap<Character,Character> mp = new HashMap<>();
+        for(int i=0;i<s.length();i++){
             char c1=s.charAt(i);
             char c2 = t.charAt(i);
 
@@ -20,7 +41,7 @@ class Solution {
                 mp.put(c1,c2);
             }
         }
-        return true;
+        return true;*/
         
 
         

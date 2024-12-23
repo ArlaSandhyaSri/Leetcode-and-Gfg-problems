7560e24358d30fdbdfab3e294cb54830058645ff
ARLA.SANDHYA SRI <140948560+ArlaSandhyaSri@users.noreply.github.com>
2024-12-23 16:53:03 +0530

    Time: 2 ms (98.62%) | Memory: 42.7 MB (27.77%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.java
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.java
@@ -19,8 +19,7 @@ class Solution {
         return sb.toString();*/
         StringBuilder sb = new StringBuilder();
         int count=0;
-        for(int i=0;i<s.length();i++){
-            char ch = s.charAt(i);
+        for(char ch:s.toCharArray()){
             if(ch=='('){
                 if(count!=0){
                     sb.append(ch);

f9a07635742b2af380169774f02b4d54ed1eae9c
ARLA.SANDHYA SRI <140948560+ArlaSandhyaSri@users.noreply.github.com>
2024-12-24 11:51:38 +0530

    Time: 2 ms (98.92%) | Memory: 45.3 MB (11.13%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -25,33 +25,6 @@ class Solution {
             }
         }
         return ans;
-        //Brute - O(N2)
-
-       /* int as[]=new int[2];
-        for(int i=0;i<nums.length;i++){
-            for(int j=i+1;j<nums.length;j++){
-                if(nums[i]+nums[j]==target){
-                    as[0]=i;
-                    as[1]=j;
-                }
-            }
-        }
-        return as;*/
-
-
-        //Better Approach
-
-       /* int ar[]=new int[2];
-        Map<Integer,Integer> mp = new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-            if(mp.containsKey(nums[i])){
-                ar[0]=i;
-                ar[1]=mp.get(nums[i]);
-                break;
-            }
-            mp.put(target-nums[i],i);
-        }
-        return ar;*/
     }
     
 }

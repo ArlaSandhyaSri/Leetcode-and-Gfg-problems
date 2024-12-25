6cc2122af91d1ca950522f559d8cf34b90b302b7
ARLA.SANDHYA SRI <140948560+ArlaSandhyaSri@users.noreply.github.com>
2024-12-25 11:24:51 +0530

    Time: 1 ms (74.91%) | Memory: 45.5 MB (80.33%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,6 +1,26 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
         int rows=matrix.length;
+        int cols=matrix[0].length;
+
+        int row[]=new int[rows];
+        int col[]=new int[cols];
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                if(matrix[i][j]==0){
+                    row[i]=-1;
+                    col[j]=-1;
+                }
+            }
+        }
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                if(row[i]==-1||col[j]==-1){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+       /* int rows=matrix.length;
         int columns = matrix[0].length;
         int ro[]=new int[rows];
         int col[]=new int[columns];
@@ -19,6 +39,6 @@ class Solution {
                     matrix[i][j]=0;
                 }
             }
-        }
+        }*/
     }
 }

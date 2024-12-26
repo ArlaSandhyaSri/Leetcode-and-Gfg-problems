51b0e8ed742d116c8435f5b1054b0b8c0f7dbe8d
ARLA.SANDHYA SRI <140948560+ArlaSandhyaSri@users.noreply.github.com>
2024-12-26 10:57:03 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (43.75%) - LeetSync

--- 48-rotate-image/rotate-image.java
+++ 48-rotate-image/rotate-image.java
@@ -22,28 +22,6 @@ class Solution {
                 c--;
             }
         }
-
-/*
-        int rows=matrix.length;
-        int columns=matrix[0].length;
-        for(int i=0;i<rows;i++){
-            for(int j=0;j<columns;j++){
-                if(i<j){
-                    int temp=matrix[i][j];
-                    matrix[i][j]=matrix[j][i];
-                    matrix[j][i]=temp;
-                }
-            }
-        }
-        for(int i=0;i<rows;i++){
-            int t=columns-1;
-            for(int j=0;j<columns/2;j++){
-                int temp=matrix[i][j];
-                matrix[i][j]=matrix[i][t];
-                matrix[i][t]=temp;
-                t--;
-            }
-        }*/
         
     }
 }

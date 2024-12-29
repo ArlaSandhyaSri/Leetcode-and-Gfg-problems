4d4ca6ef1a97d4893995341d90e5bb0afec3002c
ARLA.SANDHYA SRI <140948560+ArlaSandhyaSri@users.noreply.github.com>
2024-12-29 11:37:21 +0530

    Time: 13 ms (74.62%) | Memory: 45.3 MB (81.17%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.java
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.java
@@ -1,6 +1,30 @@
 class Solution {
     public String frequencySort(String s) {
-        StringBuilder word = new StringBuilder();
+        StringBuilder str = new StringBuilder();
+        HashMap<Character,Integer> mp = new HashMap<>();
+        for(int i=0;i<s.length();i++){
+            mp.put(s.charAt(i),mp.getOrDefault(s.charAt(i),0)+1);
+        }
+        while(!mp.isEmpty()){
+              int maximum=Integer.MIN_VALUE;
+              char mostrep='a';
+              for(Map.Entry<Character,Integer> ent:mp.entrySet()){
+                if(ent.getValue()>maximum){
+                    maximum=ent.getValue();
+                    mostrep=ent.getKey();
+                }
+              }
+              for(int i=0;i<maximum;i++){
+                str.append(mostrep);
+              }
+            mp.remove(mostrep);
+        }
+        return str.toString();
+    }
+}
+
+
+       /* StringBuilder word = new StringBuilder();
 
         HashMap<Character,Integer> mp = new HashMap<>();
         for(int i=0;i<s.length();i++){
@@ -22,4 +46,4 @@ class Solution {
         }
         return word.toString();
     }
-}
+}*/

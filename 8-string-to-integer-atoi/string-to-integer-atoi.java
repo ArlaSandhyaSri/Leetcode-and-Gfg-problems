8fbfa586b9b3cf571bedd79a7896a2bf9e1cfa05
ARLA.SANDHYA SRI <140948560+ArlaSandhyaSri@users.noreply.github.com>
2024-12-29 11:53:19 +0530

    Time: 1 ms (100.00%) | Memory: 42.3 MB (68.82%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.java
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -5,7 +5,7 @@ class Solution {
             index++;
         }
         int sign=1;
-        if(index<s.length() && s.charAt(index)=='+'){
+        if(index<s.length()&& s.charAt(index)=='+'){
             index++;
         }
         else if(index<s.length() && s.charAt(index)=='-'){
@@ -24,6 +24,32 @@ class Solution {
             index++;
         }
         return (int)result*sign;
+
+
+       /* int index=0;
+        while(index<s.length() && s.charAt(index)==' '){
+            index++;
+        }
+        int sign=1;
+        if(index<s.length() && s.charAt(index)=='+'){
+            index++;
+        }
+        else if(index<s.length() && s.charAt(index)=='-'){
+            sign=-1;
+            index++;
+        }
+        long result=0;
+        while(index<s.length() && Character.isDigit(s.charAt(index))){
+            result=result*10+(s.charAt(index)-'0');
+            if(sign*result<Integer.MIN_VALUE){
+                return Integer.MIN_VALUE;
+            }
+            else if(sign*result>Integer.MAX_VALUE){
+                return Integer.MAX_VALUE;
+            }
+            index++;
+        }
+        return (int)result*sign;*/
 
         
     }

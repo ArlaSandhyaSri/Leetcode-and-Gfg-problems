88e8f4b8d03bc2836c2cc5846eeb260505f5eaef
ARLA.SANDHYA SRI <140948560+ArlaSandhyaSri@users.noreply.github.com>
2024-12-29 11:37:33 +0530

    Time: 13 ms (74.62%) | Memory: 45.7 MB (55.10%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.java
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.java
@@ -23,27 +23,3 @@ class Solution {
     }
 }
 
-
-       /* StringBuilder word = new StringBuilder();
-
-        HashMap<Character,Integer> mp = new HashMap<>();
-        for(int i=0;i<s.length();i++){
-            mp.put(s.charAt(i),mp.getOrDefault(s.charAt(i),0)+1);
-        }
-        while(!mp.isEmpty()){
-            int maxi=Integer.MIN_VALUE;
-            char mostrep='a';
-            for(Map.Entry<Character,Integer> m : mp.entrySet()){
-                if(m.getValue()>maxi){
-                    mostrep=m.getKey();
-                    maxi=m.getValue();
-                }
-            }
-            for(int i=0;i<maxi;i++){
-                word.append(mostrep);
-            }
-            mp.remove(mostrep);
-        }
-        return word.toString();
-    }
-}*/

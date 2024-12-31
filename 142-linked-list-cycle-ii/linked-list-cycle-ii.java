fe6391bbb4bd1b9c76368cc0ba8837a453091a31
ARLA.SANDHYA SRI <140948560+ArlaSandhyaSri@users.noreply.github.com>
2024-12-31 11:26:46 +0530

    Time: 139 ms (2.39%) | Memory: 44.2 MB (90.69%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.java
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.java
@@ -11,7 +11,29 @@
  */
 public class Solution {
     public ListNode detectCycle(ListNode head) {
+        if(head==null || head.next==null){
+            return null;
+        }
         ListNode slow=head;
+        ListNode fast=head;
+        while(slow!=null && fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast){
+                break;
+            }
+        }
+        if(fast==null || fast.next==null){
+            return null;
+        }
+        slow=head;
+        while(slow!=fast){
+            slow=slow.next;
+            fast=fast.next;
+        }
+        return slow;
+
+        /*ListNode slow=head;
         ListNode fast=head;
         while(slow!=null && fast!=null && fast.next!=null){
             slow=slow.next;
@@ -25,6 +47,7 @@ public class Solution {
                 return slow;
             }
         }
-        return null;
+        return null;*/
+
     }
 }

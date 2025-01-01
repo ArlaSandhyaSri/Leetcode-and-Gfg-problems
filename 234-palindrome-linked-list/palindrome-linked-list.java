0a8c771211c31b65c6a5bc1881987e71396476b5
ARLA.SANDHYA SRI <140948560+ArlaSandhyaSri@users.noreply.github.com>
2025-01-01 11:12:15 +0530

    Time: 4 ms (83.44%) | Memory: 60.2 MB (65.74%) - LeetSync

---
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -0,0 +1,101 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+
+        if(head==null || head.next==null){
+            return true;
+        }
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        ListNode middle = slow;
+        ListNode current=middle;
+        ListNode prev=null;
+        while(current!=null){
+            ListNode next=current.next;
+            current.next=prev;
+            prev=current;
+            current=next;
+        }
+        ListNode newhead=prev;
+
+        ListNode start1=head;
+        ListNode start2=newhead;
+        while(start1!=null && start2!=null){
+            if(start1.val!=start2.val){
+                return false;
+            }
+            start1=start1.next;
+            start2=start2.next;
+        }
+        return true;
+        /*if(head==null || head.next==null){
+            return true;
+        }
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        ListNode middle=slow;
+
+        ListNode current=middle;
+        ListNode previous=null;
+        while(current!=null){
+            ListNode next=current.next;
+            current.next=previous;
+            previous=current;
+            current=next;
+        }
+        ListNode secondHalf=previous;
+
+
+        ListNode start1=head;
+        ListNode start2=secondHalf;
+        while(start1!=null && start2!=null){
+            if(start1.val!=start2.val){
+                return false;
+            }
+            start1=start1.next;
+            start2=start2.next;
+        }
+        return true;
+
+        
+    }
+    public static ListNode find(ListNode head){
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return slow;
+    }
+    public static ListNode reverse(ListNode middle){
+        ListNode current=middle;
+        ListNode previous=null;
+        while(current!=null){
+            ListNode next=current.next;
+            current.next=previous;
+            previous=current;
+            current=next;
+        }
+        return previous;*/
+
+
+    }
+}

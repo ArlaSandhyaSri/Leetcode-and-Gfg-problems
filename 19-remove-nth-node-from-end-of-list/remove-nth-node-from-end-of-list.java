dab8c9e67bdd67bba772175e9a74c0b1773f484d
ARLA.SANDHYA SRI <140948560+ArlaSandhyaSri@users.noreply.github.com>
2025-01-02 10:51:35 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (27.96%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
@@ -46,40 +46,4 @@ class Solution {
         return head;
     }
 }
-      /*  ListNode temp=head;
-        int count=0;
-        ListNode prev=head;
-        while(temp!=null){
-            count++;
-            temp=temp.next;
-        }
-        if(count<=2){
-            if(count==1){
-                head=null;
-                return null;
-            }if(count==2){
-                if(n==1){
-                    head.next=null;
-                    return head;
-                }else{
-                    head=head.next;
-                    return head;
-                }
-
-            }
-        }
-        if(count-n==0){
-            head=head.next;
-            return head;
-        }
-        temp=head;
-        for(int i=1;i<count-n;i++){
-            temp=temp.next;
-        }
-        temp.next=temp.next.next;
-        return head;
-
-
-        
-    }
-}*/
+     

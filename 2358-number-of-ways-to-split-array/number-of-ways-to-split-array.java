3b45ca1782f1e9825743b5635e3ca17756a968cc
ARLA.SANDHYA SRI <140948560+ArlaSandhyaSri@users.noreply.github.com>
2025-01-03 12:28:12 +0530

    Time: 3 ms (59.69%) | Memory: 57 MB (97.08%) - LeetSync

--- 2358-number-of-ways-to-split-array/number-of-ways-to-split-array.java
+++ 2358-number-of-ways-to-split-array/number-of-ways-to-split-array.java
@@ -1,21 +1,21 @@
 class Solution {
     public int waysToSplitArray(int[] nums) {
-        /*int n = nums.length-1;
+        int n = nums.length;
         int count=0;
-        int total=0;
+        long total=0;
+        long cursum=0;
         for(int i=0;i<n;i++){
             total+=nums[i];
         }
-        int sum=0;
         for(int i=0;i<n-1;i++){
-            sum+=nums[i];
-            if(sum>=total-sum){
+            cursum+=nums[i];
+            if(cursum>=total-cursum){
                 count++;
             }
         }
-        return count;*/
+        return count;
 
-        int n=nums.length;
+       /* int n=nums.length;
         long sumTotal=0;
         long sumCurr=0;
         int cnt=0;
@@ -33,7 +33,7 @@ class Solution {
             }
         }
 
-        return cnt;
+        return cnt;*/
        
     }
 }

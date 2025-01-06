bf8948666030b77a3f67cd9baf15fc832e647d15
ARLA.SANDHYA SRI <140948560+ArlaSandhyaSri@users.noreply.github.com>
2025-01-06 12:12:13 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (95.66%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -14,19 +14,6 @@ class Solution {
             }
         }
         return false;
-       /* int i=0;
-        int j=matrix[0].length-1;
-        while(i<matrix.length && j>=0){
-            if(target==matrix[i][j]){
-                return true;
-            }
-            else if(target>matrix[i][j]){
-                i++;
-            }else{
-                j--;
-            }
-        }
-        return false;*/
-        
+       
     }
 }

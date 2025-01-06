fd22430ba6dd756515f5568845a454c475e17ef8
ARLA.SANDHYA SRI <140948560+ArlaSandhyaSri@users.noreply.github.com>
2025-01-06 11:25:39 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (93.88%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
@@ -12,17 +12,6 @@ class Solution {
             }
         }
         return nums[start];
-       /* int l=0;
-        int h=nums.length-1;
-        while(l<h){
-            int mid=(l+h)/2;
-            if(nums[mid]>nums[h]){ //If mid is greater than end element then the smallest elements will lie between mid+1 and h only 
-                l=mid+1;
-            }else{
-                h=mid;  //else between start to mid;
-            }
-        }
-        return nums[h];*/
-        
+      
     }
 }

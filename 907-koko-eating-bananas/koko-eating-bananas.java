6199cb5def268db6e238b43ad48eb187742748bf
ARLA.SANDHYA SRI <140948560+ArlaSandhyaSri@users.noreply.github.com>
2025-01-07 11:57:19 +0530

    Time: 25 ms (44.92%) | Memory: 45.4 MB (30.32%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -32,35 +32,4 @@ class Solution {
 
     }
 }
-       /* int low=1;
-        int high=maximum(piles);
-        int ans=0;
-        while(low<=high){
-            int mid=(low+high)/2;
-            int hours=timetaken(piles,mid);
-            if(hours<=h){
-                ans=mid;
-                high=mid-1;
-            }else{
-                low=mid+1;
-            }
-        }
-        return ans;
-    }
-    static int timetaken(int piles[],int k){
-        int ans=0;
-        for(int i=0;i<piles.length;i++){
-            ans+=Math.ceil((double)piles[i]/(double)k);
-        }
-        return ans;
-    }
-    static int maximum(int piles[]){
-        int maxi=Integer.MIN_VALUE;
-        for(int i=0;i<piles.length;i++){
-            if(piles[i]>maxi){
-                maxi=piles[i];
-            }
-        }
-        return maxi;
-    }
-}*/
+       

8e1f4f1b1a14a910a6836e618ef1fc53f0848bd9
ARLA.SANDHYA SRI <140948560+ArlaSandhyaSri@users.noreply.github.com>
2025-01-14 11:45:28 +0530

    Time: 1 ms (100.00%) | Memory: 45.6 MB (40.89%) - LeetSync

--- 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.java
+++ 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[] findThePrefixCommonArray(int[] A, int[] B) {
-        int c[]=new int[A.length];
+        /*int c[]=new int[A.length];
         HashMap<Integer,Integer> mp = new HashMap<>();
         for(int i=0;i<A.length;i++){
             int count=0;
@@ -13,6 +13,23 @@ class Solution {
                 }
             }
             c[i]=count;
+        }
+        return c;*/
+
+        int[] stat = new int[A.length + 1];
+        int[] c = new int[A.length];
+
+        int cur = 0;
+        for (int i = 0; i < A.length; i++) {
+            stat[A[i]]++;
+            if (stat[A[i]] == 2) {
+                cur++;
+            }
+            stat[B[i]]++;
+            if (stat[B[i]] == 2) {
+                cur++;
+            }
+            c[i] = cur;
         }
         return c;
         

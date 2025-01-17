04d931b6f22f3f9aa5bd7a35de03c1a4cb37ab42
ARLA.SANDHYA SRI <140948560+ArlaSandhyaSri@users.noreply.github.com>
2025-01-17 11:40:57 +0530

    Time: 2 ms (30.53%) | Memory: 42.5 MB (46.21%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.java
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -22,31 +22,7 @@ class Solution {
             index++;
         }
         return (int)res*sign;
-       /* int index=0;
-        while(index<s.length() && s.charAt(index)==' '){
-            index++;
-        }
-        int sign=1;
-        if(index<s.length()&& s.charAt(index)=='+'){
-            index++;
-        }
-        else if(index<s.length() && s.charAt(index)=='-'){
-            sign=-1;
-            index++;
-        }
-        long result=0;
-        while(index<s.length() && Character.isDigit(s.charAt(index))){
-            result=result*10+(s.charAt(index)-'0');
-            if(sign*result<Integer.MIN_VALUE){
-                return Integer.MIN_VALUE;
-            }
-            else if(sign*result>Integer.MAX_VALUE){
-                return Integer.MAX_VALUE;
-            }
-            index++;
-        }
-        return (int)result*sign;*/
-
+      
 
        
     }

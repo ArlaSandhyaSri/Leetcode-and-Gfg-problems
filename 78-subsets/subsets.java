3277035b1e1bb8bac394018ca1bec96bc88cef79
ARLA.SANDHYA SRI <140948560+ArlaSandhyaSri@users.noreply.github.com>
2025-01-18 11:54:34 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (44.57%) - LeetSync

---
+++ 78-subsets/subsets.java
@@ -0,0 +1,18 @@
+class Solution {
+    List<List<Integer>> finall=new ArrayList<>();
+    public List<List<Integer>> subsets(int[] nums) {
+        subsets(nums,0,new ArrayList<>());
+        return finall;
+    }
+    public void subsets(int nums[],int index,ArrayList<Integer>temp){
+        if(index>=nums.length){
+            finall.add(new ArrayList(temp));
+            return;
+
+        }
+        temp.add(nums[index]);
+        subsets(nums,index+1,temp);
+        temp.remove(temp.size()-1);
+        subsets(nums,index+1,temp);
+    }
+}

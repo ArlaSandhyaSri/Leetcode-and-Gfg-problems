67368d46fb83b28879131b051309e217e7a19aee
ARLA.SANDHYA SRI <140948560+ArlaSandhyaSri@users.noreply.github.com>
2025-01-20 12:24:25 +0530

    Time: 4 ms (66.55%) | Memory: 43.3 MB (53.72%) - LeetSync

--- 414-third-maximum-number/third-maximum-number.java
+++ 414-third-maximum-number/third-maximum-number.java
@@ -4,15 +4,14 @@ class Solution {
         int max=Integer.MIN_VALUE;
         int count=1;
         for(int i=nums.length-2;i>=0;i--){
-            if(nums[i]==nums[i+1]){
-                continue;
-            }
-            else{
+            if (nums[i] != nums[i + 1]) {
                 count++;
+                if (count == 3) {
+                    return nums[i];
+                }
+          
             }
-            if(count==3){
-                return nums[i];
-            }
+            
 
         }
         return nums[nums.length-1];

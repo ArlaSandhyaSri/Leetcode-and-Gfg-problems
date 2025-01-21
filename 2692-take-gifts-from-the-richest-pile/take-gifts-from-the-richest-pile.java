b9fe8516f04425d3ec62c8e5a488a5cf9051dc84
ARLA.SANDHYA SRI <140948560+ArlaSandhyaSri@users.noreply.github.com>
2025-01-21 21:54:43 +0530

    Time: 28 ms (6.14%) | Memory: 44.9 MB (5.80%) - LeetSync

--- 2692-take-gifts-from-the-richest-pile/take-gifts-from-the-richest-pile.java
+++ 2692-take-gifts-from-the-richest-pile/take-gifts-from-the-richest-pile.java
@@ -1,5 +1,6 @@
 class Solution {
     public long pickGifts(int[] gifts, int k) {
+        //POTD --it will have less tc and sc when we  use priority queue
         int n = gifts.length;
         for(int i=0;i<k;i++){
             Arrays.sort(gifts);

9c10767ce7f59865a81235214c8447e75f3bb7c9
ARLA.SANDHYA SRI <140948560+ArlaSandhyaSri@users.noreply.github.com>
2025-01-27 11:52:19 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (59.27%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,6 +1,6 @@
 class Solution {
     public double myPow(double x, int n) {
-        /*double answer=1.0;
+        double answer=1.0;
         long power=n;
         if(power<0){
             power=-1*power;
@@ -20,26 +20,6 @@ class Solution {
         if(n<0){
             answer=1/answer;
         }
-        return answer;*/
-        double ans = 1.0;
-        long power=n;
-        if(power<0){
-            power=-1*power;
-        }
-        while(power>0){
-            if(power%2==1){
-                ans*=x;
-                power-=1;
-            }
-            else{
-                x=x*x;
-                power/=2;
-            }
-        }
-        if(n<0){
-            ans=1/ans;
-        }
-        return ans;
+        return answer;
     }
-
 }

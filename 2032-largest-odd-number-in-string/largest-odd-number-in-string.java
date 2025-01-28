a46769db5ee908f58f279aa28b616d4ece48f9c0
ARLA.SANDHYA SRI <140948560+ArlaSandhyaSri@users.noreply.github.com>
2025-01-28 08:58:20 +0530

    Time: 1 ms (99.64%) | Memory: 45.2 MB (78.06%) - LeetSync

--- 2032-largest-odd-number-in-string/largest-odd-number-in-string.java
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.java
@@ -1,5 +1,6 @@
 class Solution {
     public String largestOddNumber(String num) {
+        //if we should not use inbuilt functions like substring and all then main index variable and if found odd store that index and later append from 0 to index.
         for(int i=num.length()-1;i>=0;i--){
             if((num.charAt(i)-'0')%2!=0){
                 return num.substring(0,i+1);

3d113c67eb17844139042122647b901393ccc88e
ARLA.SANDHYA SRI <140948560+ArlaSandhyaSri@users.noreply.github.com>
2025-02-01 10:35:44 +0530

    Time: 0 ms (100.00%) | Memory: 44.5 MB (78.12%) - LeetSync

--- 328-odd-even-linked-list/odd-even-linked-list.java
+++ 328-odd-even-linked-list/odd-even-linked-list.java
@@ -26,22 +26,6 @@ class Solution {
         }
         odd.next=evenhead;
         return head;
-        /*
-        if(head==null || head.next==null){
-            return head;
-        }
-        ListNode odd=head;
-        ListNode even=head.next;
-        ListNode evenhead=head.next;
-
-        while(odd!=null && odd.next!=null && even!=null && even.next!=null){
-            odd.next=odd.next.next;
-            odd=odd.next;
-            even.next=even.next.next;
-            even=even.next;
-        }
-        odd.next=evenhead;
-        return head;*/
         
     }
 }

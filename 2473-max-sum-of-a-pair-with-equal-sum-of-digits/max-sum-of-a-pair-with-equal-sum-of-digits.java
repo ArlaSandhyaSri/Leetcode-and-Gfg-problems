a00862211df2cfc2c0d3b7de6ad1b02cd6eee06f
ARLA.SANDHYA SRI <140948560+ArlaSandhyaSri@users.noreply.github.com>
2025-02-12 08:57:54 +0530

    Time: 22 ms (90.86%) | Memory: 55.9 MB (76.45%) - LeetSync

--- 2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.java
+++ 2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.java
@@ -1,6 +1,23 @@
 class Solution {
     public int maximumSum(int[] nums) {
-        int maxsum=-1;
+        int[] storage = new int[90];
+        int max = -1;
+        
+        for(int num : nums){
+            int sumOfDigit = sum(num);
+            if(storage[sumOfDigit] != 0){
+                int localSum = storage[sumOfDigit] + num;
+                max = Math.max(max, localSum);
+                storage[sumOfDigit] = Math.max(num, storage[sumOfDigit]);
+            } else {
+                storage[sumOfDigit] = num;
+            }
+        }
+        return max;
+    }
+
+        //O(n)
+      /*  int maxsum=-1;
         HashMap<Integer,Integer> mp = new HashMap<>();
         for(int i=0;i<nums.length;i++){
             int n=sum(nums[i]);
@@ -10,7 +27,10 @@ class Solution {
             mp.put(n,Math.max(mp.getOrDefault(n,0),nums[i]));
         }
         return maxsum;
-    }
+    }*/
+
+
+    //O(n2)-TLE
         /*int maxisum=-1;
         for(int i=0;i<nums.length-1;i++){
             int first=sum(nums[i]);

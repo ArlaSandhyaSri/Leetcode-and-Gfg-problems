a762fa298847c27dea021734308b15798341d726
ARLA.SANDHYA SRI <140948560+ArlaSandhyaSri@users.noreply.github.com>
2025-02-21 08:42:49 +0530

    Time: 1 ms (93.02%) | Memory: 44.6 MB (46.23%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -21,34 +21,5 @@ class Solution {
             }
         }
     }
-        //Approach-2  SC-->O(1)
-       /* List<List<Integer>> ans = new ArrayList<>();
-        
-
-
-        //Approach-1   SC-->O(N)
-       /* List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> ar = new ArrayList<>();
-        boolean freq[]=new boolean[nums.length];
-
-        permutations(nums,ar,ans,freq);
-        return ans;
-        
-    }
-    static void permutations(int nums[],List<Integer>ar,List<List<Integer>>ans,boolean freq[]){
-        if(ar.size()==nums.length){
-            ans.add(new ArrayList<>(ar));
-            return;
-        }
-        for(int i=0;i<nums.length;i++){
-            if(!freq[i]){
-                freq[i]=true;
-                ar.add(nums[i]);
-                permutations(nums,ar,ans,freq);
-                ar.remove(ar.size()-1);
-                freq[i]=false;
-            }
-        }
-    }*/
 
 }

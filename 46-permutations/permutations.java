53c781252c918b46e70bd7988a6abd6368669806
ARLA.SANDHYA SRI <140948560+ArlaSandhyaSri@users.noreply.github.com>
2025-02-21 08:42:05 +0530

    Time: 1 ms (93.02%) | Memory: 44.5 MB (81.07%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,6 +1,33 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>();
+        List<List<Integer>>ans = new ArrayList<>();
+        List<Integer> ar = new ArrayList<>();
+        boolean freq[]=new boolean[nums.length];
+        permutations(nums,ans,ar,freq);
+        return ans;
+    }
+    static void permutations(int nums[],List<List<Integer>> ans,List<Integer>ar,boolean freq[]){
+        if(ar.size()==nums.length){
+            ans.add(new ArrayList<>(ar));
+            return;
+        }
+        for(int i=0;i<nums.length;i++){
+            if(!freq[i]){
+                freq[i]=true;
+                ar.add(nums[i]);
+                permutations(nums,ans,ar,freq);
+                ar.remove(ar.size()-1);
+                freq[i]=false;
+            }
+        }
+    }
+        //Approach-2  SC-->O(1)
+       /* List<List<Integer>> ans = new ArrayList<>();
+        
+
+
+        //Approach-1   SC-->O(N)
+       /* List<List<Integer>> ans = new ArrayList<>();
         List<Integer> ar = new ArrayList<>();
         boolean freq[]=new boolean[nums.length];
 
@@ -22,5 +49,6 @@ class Solution {
                 freq[i]=false;
             }
         }
-    }
+    }*/
+
 }

4332b7e3afa7a9a162fcfa6f704f29ea6b4c89fb
ARLA.SANDHYA SRI <140948560+ArlaSandhyaSri@users.noreply.github.com>
2025-03-19 08:50:10 +0530

    Time: 2 ms (97.67%) | Memory: 42 MB (20.54%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> st = new Stack<>();
-        for(int i=0;i<s.length();i++){
+        /*for(int i=0;i<s.length();i++){
             if(s.charAt(i)=='('||s.charAt(i)=='{'||s.charAt(i)=='['){
                 st.push(s.charAt(i));
             }else if(s.charAt(i)==')'){
@@ -32,7 +32,26 @@ class Solution {
         if(st.size()==0){
             return true;
         }
+        return false;*/
+        for(char ch:s.toCharArray()){
+            if(ch==')' || ch=='}'||ch==']'){
+                if(st.size()==0){
+                    return false;
+                }
+                char el=st.pop();
+                if((ch==')' && el!='(') || (ch=='}'&&el!='{') || (ch==']'&&el!='[')){
+                    return false;
+                }
+
+            }else{
+                st.push(ch);
+            }
+        }
+        if(st.size()==0){
+            return true;
+        }
         return false;
     }
+    
 }
         

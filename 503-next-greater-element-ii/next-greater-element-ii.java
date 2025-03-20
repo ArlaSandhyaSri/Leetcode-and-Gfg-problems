b5938d130204348aa6fd33c76791e9813762f500
ARLA.SANDHYA SRI <140948560+ArlaSandhyaSri@users.noreply.github.com>
2025-03-20 10:50:58 +0530

    Time: 17 ms (48.01%) | Memory: 46.4 MB (48.93%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.java
+++ 503-next-greater-element-ii/next-greater-element-ii.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
-        int ans[]=new int[nums.length];
+        /*int ans[]=new int[nums.length];
         for(int i=0;i<nums.length;i++){
             ans[i]=Integer.MIN_VALUE;
             for(int j=i+1;j<nums.length;j++){
@@ -22,7 +22,26 @@ class Solution {
             }
 
         }
+        return ans;*/
+
+        //Optimized
+        Stack<Integer> st = new Stack<>();
+        int ans[]=new int[nums.length];
+        for(int i=(2*nums.length)-1;i>=0;i--){
+            while(!st.isEmpty() && st.peek()<=nums[i%nums.length]){
+                st.pop();
+            }
+            if(i<nums.length){
+                if(!st.isEmpty()){
+                    ans[i]=st.peek();
+                }else{
+                    ans[i]=-1;
+                }
+            }
+            st.push(nums[i%nums.length]);
+        }
         return ans;
+
         
     }
 }

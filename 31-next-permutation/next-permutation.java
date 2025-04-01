6f229b55048517879581367c184d19e8754cb486
ARLA.SANDHYA SRI <140948560+ArlaSandhyaSri@users.noreply.github.com>
2025-04-01 13:17:19 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (11.47%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -40,42 +40,5 @@ class Solution {
     }
 
 
-        /*int n=nums.length;
-        int index1=-1;
-        int index2=-1;
-        for(int i=n-2;i>=0;i--){
-            if(nums[i]<nums[i+1]){
-                index1=i;
-                break;
-            }
-        }
-        if(index1==-1){
-            reverse(nums,0,n);  //// if no next possible exit then return first which is reverse of this
-        }
-        else{
-            for(int i=n-1;i>=0;i--){
-                if(nums[i]>nums[index1]){
-                    index2=i;
-                    break;
-                }
-            }
-            swap(nums,index1,index2);
-            reverse(nums,index1+1,n);
-        }
-    }
-    static void swap(int nums[],int from,int to){
-        int temp=nums[from];
-        nums[from]=nums[to];
-        nums[to]=temp;
-    }
-    static void reverse(int nums[],int start,int end){
-        end=end-1;
-        while(start<=end){
-            int temp=nums[start];
-            nums[start]=nums[end];
-            nums[end]=temp;
-            start++;
-            end--;
-        }
-    }*/
+        
 }  

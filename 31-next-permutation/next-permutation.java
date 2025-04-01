d191a1d064472c83011bf1fff1feab7034c7964c
ARLA.SANDHYA SRI <140948560+ArlaSandhyaSri@users.noreply.github.com>
2025-04-01 13:17:05 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (36.78%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -10,6 +10,46 @@ class Solution {
             }
         }
         if(index1==-1){
+            reverse(nums,0,n);
+        }else{
+            for(int i=n-1;i>=0;i--){
+                if(nums[i]>nums[index1]){
+                    index2=i;
+                    break;
+                }
+            }
+            swap(nums,index1,index2);
+            reverse(nums,index1+1,n);
+        }
+    }
+    static void swap(int nums[],int i,int j){
+        int temp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
+    }
+    static void reverse(int nums[],int i,int n){
+        int l=i;
+        int h=n-1;
+        while(l<h){
+            int temp=nums[l];
+            nums[l]=nums[h];
+            nums[h]=temp;
+            l++;
+            h--;
+        }
+    }
+
+
+        /*int n=nums.length;
+        int index1=-1;
+        int index2=-1;
+        for(int i=n-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                index1=i;
+                break;
+            }
+        }
+        if(index1==-1){
             reverse(nums,0,n);  //// if no next possible exit then return first which is reverse of this
         }
         else{
@@ -37,5 +77,5 @@ class Solution {
             start++;
             end--;
         }
-    }
+    }*/
 }  

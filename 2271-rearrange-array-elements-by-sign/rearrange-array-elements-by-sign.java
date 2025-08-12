54da5664097613904cc380257cf75c8348c02daa
ARLA.SANDHYA SRI <140948560+ArlaSandhyaSri@users.noreply.github.com>
2025-08-12 15:30:26 +0530

    Time: 3 ms (100.00%) | Memory: 85.3 MB (11.31%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
@@ -1,47 +1,14 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-        int n=nums.length;
-
-        //Brute
-
-        /*ArrayList<Integer> pos = new ArrayList<>();
-        ArrayList<Integer> neg = new ArrayList<>();
-
-        for(int i=0;i<n;i++){
-            if(nums[i]>=0){
-                pos.add(nums[i]);
-            }else{
-                neg.add(nums[i]);
-            }
-        }
-        int arr[]=new int[n];
-        int i=0;
-        int j=0;
-        int k=0;
-        while(i<pos.size() && j<neg.size()){
-            if(k%2==0){
-                arr[k++]=pos.get(i++);
-            }else{
-                arr[k++]=neg.get(j++);
-            }
-        }
-        while(i<pos.size()){
-            arr[k++]=pos.get(i++);
-        }while(j<neg.size()){
-            arr[k++]=neg.get(j++);
-        }
-        return arr;*/
-
-        //Optimal
-
-        int arr[]=new int[n];
+        int arr[]=new int[nums.length];
         int pos=0;
         int neg=1;
-        for(int i=0;i<n;i++){
+        for(int i=0;i<nums.length;i++){
             if(nums[i]>0){
                 arr[pos]=nums[i];
                 pos+=2;
-            }else{
+            }
+            else if(nums[i]<0){
                 arr[neg]=nums[i];
                 neg+=2;
             }

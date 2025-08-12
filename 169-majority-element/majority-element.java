4c5bb68ee2ca2610dfe6e120da9a77a83e0df94c
ARLA.SANDHYA SRI <140948560+ArlaSandhyaSri@users.noreply.github.com>
2025-08-12 12:28:45 +0530

    Time: 1422 ms (5.14%) | Memory: 53.2 MB (49.51%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,57 +1,20 @@
 class Solution {
-    public int majorityElement(int[] nums) {        
-        //BruteForce
-        /*int maj=-1;
-        for(int i=0;i<nums.length;i++){
+    public int majorityElement(int[] nums) {
+        int n=nums.length;
+        int major=-1;
+        for(int i=0;i<n;i++){
             int count=0;
-            for(int j=0;j<nums.length;j++){
+            for(int j=0;j<n;j++){
                 if(nums[i]==nums[j]){
                     count++;
                 }
             }
-            if(count>nums.length/2){
+            if(count>n/2){
                 return nums[i];
             }
-        }
-        return -1;*/
 
-        //Better
+        }
 
-        /*HashMap<Integer,Integer> mp = new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-            mp.put(nums[i],mp.getOrDefault(nums[i],0)+1);
-        }
-        for(Map.Entry<Integer,Integer> entry : mp.entrySet()){
-            if(entry.getValue()>nums.length/2){
-                return entry.getKey();
-            }
-        }
-        return -1;*/
-
-        //Optimal --> using moore voting algo
-        int element=-1;
-        int count=0;
-        for(int i=0;i<nums.length;i++){
-            if(count==0){
-                element=nums[i];
-                count++;
-            }
-            else if(element==nums[i]){
-                count++;
-            }
-            else{
-                count--;
-            }
-        }
-        int c=0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==element){
-                c++;
-            }
-        }
-        if(c>nums.length/2){
-            return element;
-        }
         return -1;
     }
 }

044d23013c23d7367bb2ecea018bb51192fe63b2
ARLA.SANDHYA SRI <140948560+ArlaSandhyaSri@users.noreply.github.com>
2025-11-04 07:47:00 +0530

    Time: 2 ms (75.40%) | Memory: 55.5 MB (6.01%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,16 +1,45 @@
 class Solution {
     public int majorityElement(int[] nums) {
+
         int n=nums.length;
-        HashMap<Integer,Integer> mp = new HashMap<Integer,Integer>();
-        
+        int element=-1;
+        int count=0;
+
         for(int i=0;i<n;i++){
-            mp.put(nums[i],mp.getOrDefault(nums[i],0)+1);
-        } 
-        for(Map.Entry<Integer,Integer> et:mp.entrySet())    {
-            if(et.getValue()>n/2){
-                return et.getKey();
+            if(count==0){
+                element=nums[i];
+                count++;
             }
-        }  
+            else if(nums[i]==element){
+                count++;
+        
+            }else{
+                count--;
+            }
+        }
+
+        int c=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]==element){
+                c++;
+            }
+        }
+
+        if(c>n/2){
+            return element;
+        }
         return -1;
+
+        // HashMap<Integer,Integer> mp = new HashMap<Integer,Integer>();
+        
+        // for(int i=0;i<n;i++){
+        //     mp.put(nums[i],mp.getOrDefault(nums[i],0)+1);
+        // } 
+        // for(Map.Entry<Integer,Integer> et:mp.entrySet())    {
+        //     if(et.getValue()>n/2){
+        //         return et.getKey();
+        //     }
+        // }  
+        // return -1;
     }
 }

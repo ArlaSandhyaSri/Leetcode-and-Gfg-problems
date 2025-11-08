45e605fbd29fd0275e9ee65a89ae5c33c144433a
ARLA.SANDHYA SRI <140948560+ArlaSandhyaSri@users.noreply.github.com>
2025-11-08 11:42:35 +0530

    Time: 936 ms (5.07%) | Memory: 52.4 MB (5.86%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,49 +1,28 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-
         int n=nums.length;
-        int count1=0;
-        int count2=0;
-        int element1=Integer.MIN_VALUE;
-        int element2=Integer.MIN_VALUE;
-        for(int i=0;i<nums.length;i++){
-            if(count1==0 && element2!=nums[i]){
-                count1++;
-                element1=nums[i];
+        List<Integer> arr = new ArrayList<>();
+        for(int i=0;i<n;i++){
+            if(arr.size()==0 || arr.get(0)!=nums[i]){
+                int count=0;
+                for(int j=0;j<n;j++){
+                    if(nums[i]==nums[j]){
+                        count++;
+                    }
+                }
+                if(count>(n/3)){
+                    arr.add(nums[i]);
+                }
+
+                if(arr.size()==2){
+                    break;
+
+                }
             }
-            else if(count2==0 && element1!=nums[i]){
-                count2++;
-                element2=nums[i];
-            }
-            else if(nums[i]==element1){
-                count1++;
-            }
-            else if(nums[i]==element2){
-                count2++;
-            }
-            else{
-                count1--;
-                count2--;
-            }
+            
         }
-        count1=0;
-        count2=0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==element1){
-                count1++;
-            }
-            else if(nums[i]==element2){
-                count2++;
-            }
-        }
-        ArrayList<Integer> ar = new ArrayList<>();
-        if(count1>n/3){
-            ar.add(element1);
-        }
-        if(count2>n/3){
-            ar.add(element2);
-        }
-        return ar;
-       
+
+        return arr;
+        
     }
 }

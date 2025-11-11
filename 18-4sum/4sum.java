0aadd4559c5246a4012467a654c99fad4f6bcdec
ARLA.SANDHYA SRI <140948560+ArlaSandhyaSri@users.noreply.github.com>
2025-11-11 07:37:26 +0530

    Time: 19 ms (46.21%) | Memory: 45.4 MB (21.77%) - LeetSync

--- 18-4sum/4sum.java
+++ 18-4sum/4sum.java
@@ -1,26 +1,30 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        List<List<Integer>> pair=new ArrayList<>();
         int n=nums.length;
+        List<List<Integer>> finall = new ArrayList<>();
         Arrays.sort(nums);
         for(int i=0;i<nums.length;i++){
             if(i!=0 && nums[i]==nums[i-1]){
                 continue;
             }
-            for(int j=i+1;j<nums.length;j++){
+            for(int j=i+1;j<n;j++){
                 if(j>i+1 && nums[j]==nums[j-1]){
                     continue;
                 }
-                int k = j+1;
-                int l = nums.length-1;
+
+                int k=j+1;
+                int l=n-1;
+
                 while(k<l){
                     long sum = nums[i];
                     sum+=nums[j];
                     sum+=nums[k];
                     sum+=nums[l];
+
                     if(sum<target){
                         k++;
-                    }else if(sum>target){
+                    }
+                    else if(sum>target){
                         l--;
                     }
                     else{
@@ -29,7 +33,8 @@ class Solution {
                         ar.add(nums[j]);
                         ar.add(nums[k]);
                         ar.add(nums[l]);
-                        pair.add(ar);
+                        finall.add(ar);
+
                         k++;
                         l--;
                         while(k<l && nums[k]==nums[k-1]){
@@ -38,11 +43,14 @@ class Solution {
                         while(k<l && nums[l]==nums[l+1]){
                             l--;
                         }
+
+                        
+
                     }
                 }
             }
         }
-        return pair;
+        return finall;
 
         
     }

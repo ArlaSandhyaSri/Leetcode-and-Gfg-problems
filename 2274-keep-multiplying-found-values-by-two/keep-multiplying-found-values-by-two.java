3339ea2acc36e43bd62f1f7e01190c6b6eaaef8c
ARLA.SANDHYA SRI <140948560+ArlaSandhyaSri@users.noreply.github.com>
2025-11-19 21:10:54 +0530

    Time: 7 ms (7.96%) | Memory: 45.9 MB (17.84%) - LeetSync

--- 2274-keep-multiplying-found-values-by-two/keep-multiplying-found-values-by-two.java
+++ 2274-keep-multiplying-found-values-by-two/keep-multiplying-found-values-by-two.java
@@ -1,27 +1,5 @@
 class Solution {
     public int findFinalValue(int[] nums, int original) {
-    //     int n=nums.length;
-    //     Arrays.sort(nums);
-    //     boolean found = true;
-    //     while(found){
-    //         found = false;
-    //         int st = 0;
-    //         int end = n-1;
-    //         while(st<=end){
-    //             int mid=(st+end)/2;
-    //             if(nums[mid]==original){
-    //                 original=2*original;
-    //                 found = true;
-    //             }else if(nums[mid]<original){
-    //                 st=mid+1;
-    //             }else{
-    //                 end=mid-1;
-    //             }
-    //         }
-    //     }
-    //     return original;
-    // }
-
         int n=nums.length;
         boolean found = true;
         while(found){
@@ -32,20 +10,6 @@ class Solution {
         }
         return original;
     }
-        //     for(int i=0;i<n;i++){
-        //         if(nums[i]==original){
-        //             found=true;
-        //         }
-        //     }
-        //     if(found){
-        //         original=2*original;
-        //     }
-
-        // }
-        // return original;
-        
-    
-
     static boolean valuefound(int nums[],int original){
         Arrays.sort(nums);
         int n=nums.length;

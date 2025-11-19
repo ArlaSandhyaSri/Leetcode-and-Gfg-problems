9ce3760ccdf275ac8847d2fd763b0a170d9cfabc
ARLA.SANDHYA SRI <140948560+ArlaSandhyaSri@users.noreply.github.com>
2025-11-19 21:04:15 +0530

    Time: 1 ms (93.95%) | Memory: 45.3 MB (26.81%) - LeetSync

--- 2274-keep-multiplying-found-values-by-two/keep-multiplying-found-values-by-two.java
+++ 2274-keep-multiplying-found-values-by-two/keep-multiplying-found-values-by-two.java
@@ -1,41 +1,42 @@
 class Solution {
     public int findFinalValue(int[] nums, int original) {
-        int n=nums.length;
-        Arrays.sort(nums);
-        boolean found = true;
-        while(found){
-            found = false;
-            int st = 0;
-            int end = n-1;
-            while(st<=end){
-                int mid=(st+end)/2;
-                if(nums[mid]==original){
-                    original=2*original;
-                    found = true;
-                }else if(nums[mid]<original){
-                    st=mid+1;
-                }else{
-                    end=mid-1;
-                }
-            }
-        }
-        return original;
-    }
-}
     //     int n=nums.length;
+    //     Arrays.sort(nums);
     //     boolean found = true;
     //     while(found){
     //         found = false;
-    //         for(int i=0;i<n;i++){
-    //             if(nums[i]==original){
-    //                 found=true;
+    //         int st = 0;
+    //         int end = n-1;
+    //         while(st<=end){
+    //             int mid=(st+end)/2;
+    //             if(nums[mid]==original){
+    //                 original=2*original;
+    //                 found = true;
+    //             }else if(nums[mid]<original){
+    //                 st=mid+1;
+    //             }else{
+    //                 end=mid-1;
     //             }
     //         }
-    //         if(found){
-    //             original=2*original;
-    //         }
-
     //     }
     //     return original;
-        
     // }
+
+        int n=nums.length;
+        boolean found = true;
+        while(found){
+            found = false;
+            for(int i=0;i<n;i++){
+                if(nums[i]==original){
+                    found=true;
+                }
+            }
+            if(found){
+                original=2*original;
+            }
+
+        }
+        return original;
+        
+    }
+}

f84d16fd835b90dd037ea5342f0b924ba838fb68
ARLA.SANDHYA SRI <140948560+ArlaSandhyaSri@users.noreply.github.com>
2025-12-01 07:54:12 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (32.99%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -4,11 +4,8 @@ class Solution {
 
         m--;
         n--;
-        while(k>=0){
-            if(m>=0 && n>=0 && nums1[m]<=nums2[n]){
-                nums1[k--]=nums2[n--];
-            }
-            else if(m>=0){
+        while(n>=0){
+            if(m>=0 && nums1[m]>nums2[n]){
                 nums1[k--]=nums1[m--];
             }
             else{

7c718338d0401d4794f05785b6f4d0936fa52398
ARLA.SANDHYA SRI <140948560+ArlaSandhyaSri@users.noreply.github.com>
2025-12-03 06:57:17 +0530

    Time: 1 ms (94.85%) | Memory: 47.6 MB (7.73%) - LeetSync

---
+++ 724-find-pivot-index/find-pivot-index.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int suf=0;
+        for(int num:nums){
+            suf+=num;
+        }
+        int pref=0;
+        for(int i=0;i<nums.length;i++){
+            suf-=nums[i];
+            if(pref==suf){
+                return i;
+            }
+
+            pref+=nums[i];
+            
+
+        }
+        return -1;
+        
+    }
+}

a88386608862b35e6cf9ed4a67ffb6c44cc23f53
ARLA.SANDHYA SRI <140948560+ArlaSandhyaSri@users.noreply.github.com>
2026-02-09 19:45:14 +0530

    Time: 1 ms (90.76%) | Memory: 47.6 MB (12.42%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.java
+++ 152-maximum-product-subarray/maximum-product-subarray.java
@@ -2,36 +2,36 @@ class Solution {
     public int maxProduct(int[] nums) {
         int max_product = Integer.MIN_VALUE;
         int n=nums.length;
-        for(int i=0;i<nums.length;i++){
-            int product = 1;
-            for(int j=i;j<nums.length;j++){
-                product*=nums[j];
-                max_product = Math.max(max_product,product);
-            }
-
-        }
-        return max_product;
-
-        // int pref=1;
-        // int suf=1;
-
         // for(int i=0;i<nums.length;i++){
-        //     if(pref==0){
-        //         pref=1;
+        //     int product = 1;
+        //     for(int j=i;j<nums.length;j++){
+        //         product*=nums[j];
+        //         max_product = Math.max(max_product,product);
         //     }
-        //     if(suf==0){
-        //         suf=1;
-        //     }
-
-        //     pref*=nums[i];
-        //     suf*=nums[n-i-1];
-
-        //     max_product = Math.max(max_product,Math.max(pref,suf));
-
-
 
         // }
         // return max_product;
+
+        int pref=1;
+        int suf=1;
+
+        for(int i=0;i<nums.length;i++){
+            if(pref==0){
+                pref=1;
+            }
+            if(suf==0){
+                suf=1;
+            }
+
+            pref*=nums[i];
+            suf*=nums[n-i-1];
+
+            max_product = Math.max(max_product,Math.max(pref,suf));
+
+
+
+        }
+        return max_product;
         
     }
 }

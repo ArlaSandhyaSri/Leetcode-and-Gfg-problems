964208a79f660e908edbc8c5ce5cd5d8145cd9a0
ARLA.SANDHYA SRI <140948560+ArlaSandhyaSri@users.noreply.github.com>
2026-02-16 22:05:17 +0530

    Time: 6 ms (23.95%) | Memory: 47.9 MB (10.99%) - LeetSync

--- 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.java
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.java
@@ -5,18 +5,25 @@ class Solution {
         for(int i=0;i<grid.length;i++){
             for(int j=0;j<grid[0].length;j++){
                 mp.put(grid[i][j],mp.getOrDefault(grid[i][j],0)+1);
+                
             }
         }
+
         for(int i=1;i<=grid.length*grid.length;i++){
             if(!mp.containsKey(i)){
                 ans[1]=i;
-            }else{
+            }
+
+            else{
                 if(mp.get(i)>1){
                     ans[0]=i;
                 }
             }
         }
+
         return ans;
+
+        
         
     }
 }
